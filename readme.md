0418d6f4670c47fadc44e8e7ba41b7b9ee13d1e3
Stefano Bernardo <stefano.berna8@gmail.com>
2024-02-16 20:37:56 +0100

    Rev. 1.0

---
+++ readme.md
@@ -0,0 +1,35 @@
+# Book Shop
+
+## 1.0 Consegna
+
+    1.1 
+
+    - usa bootstrap per creare un layout veloce
+    - mostra tutti gli utenti in una tabella bootstrap partendo dall'API. usa ASYNC AWAIT
+    - crea un dropdown con tre opzioni: "nome", "username" e "email". Il dropdown sarà la prima parte del tuo filtro
+    - crea un input di testo, quando l'utente scrive qualcosa la risposta dell'API dovrebbe venir filtrata e renderizzata usando sia il valore dell'input che l'opzione 
+      selezionata nel dropdown
+    
+    Tips:
+    oggetto["proprietà"]
+    const propDaControllare ="username"
+    oggetto[propDaControllare]
+
+## 2.0 Risorse
+
+    2.1 API: https://jsonplaceholder.typicode.com/users
+
+    2.2 Librerie/Framework
+        2.2.1 Bootstrap
+            2.2.1.1 CSS: <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+            2.2.1.2 POPPER: <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+            2.2.1.3 JS: <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+        2.2.2 Fontawesome
+            2.2.2.1 <script src="https://kit.fontawesome.com/5134816b72.js" crossorigin="anonymous"></script>
+        2.2.3 Google Fonts
+
+## 3.0 Procedimenti
+
+    3.1 Funzioni
+
+
---
+++ script.js
@@ -0,0 +1,64 @@
+//Variabili globali
+
+//Corpo della tabella
+const tableBody = document.getElementById("tablebody");
+
+//Input di ricerca
+const searchInput = document.getElementById("search-input").value;
+
+//Call di fetch globale con creazione iniziale della tabella
+window.onload = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const users = await response.json();
+        createTable(users);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//Funzione di creazione della tabella
+
+function createTable (users) {
+
+    let count = 1;
+    users.forEach(user => {
+        let newRow = document.createElement("tr");
+        let num = document.createElement("td");
+        num.innerText = count;
+        let name = document.createElement("td");
+        name.innerText = user.name;
+        let username = document.createElement("td");
+        username.innerText = user.username;
+        let email = document.createElement("td");
+        email.innerText = user.email;
+
+        tableBody.appendChild(newRow);
+            newRow.appendChild(num);
+            newRow.appendChild(name);
+            newRow.appendChild(username);
+            newRow.appendChild(email);
+
+        count++;
+    });
+}
+
+//Funzione di filtraggio
+
+
+function filterUsers () {
+
+    //Parametro di filtraggio
+    const searchParam = document.getElementById("param-selection").value;
+    let filteredResults = [];
+
+    if (searchParam === "Name") {
+        console.log(searchParam);
+    } else if (searchParam === "Username") {
+        console.log(searchParam);
+    } else if (searchParam === "e-Mail") {
+        console.log(searchParam);
+    }
+
+    createTable(filteredResults);
+}
